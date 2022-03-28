bef6350c5221a5d5e17ea7707cc347263c44e344
MD. SAIFUDDIN <57839548+satuhin95@users.noreply.github.com>
2022-03-28 17:14:21 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        'bookmarks-purple': '#5267DF',
+        'bookmarks-red': '#FA5959',
+        'bookmarks-blue': '#242A45',
+        'bookmarks-grey': '#9194A2',
+        'bookmarks-white': '#f7f7f7',
+      },
+    },
+    fontFamily:{
+      'Poppins':['Poppins', 'sans-serif']
+    },
+    container: {
+      center: true,
+      padding:"1rem",
+      screen:{
+        lg:"1124px",
+        xl:"1124px",
+        "2xl":"1124px",
+      }
+    },
+  },
+  plugins: [],
+}
